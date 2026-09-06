ce2c2ebb44f52bd2b1e5d779ade1b339bc84e3bb
ppop11752-code <ppop11752@gmail.com>
2026-09-06 18:38:35 +0700

    Delete server/backend/server/backend directory

--- server/backend/server/backend/src/queue.ts
+++
@@ -1,1 +0,0 @@
-export function insertAtQueueEnd(q:string[],id:string){return q.includes(id)?q:[...q,id]} export function popQueue(q:string[]){return q.length?{playerId:q[0]!,queue:q.slice(1)}:{playerId:null,queue:q}}
